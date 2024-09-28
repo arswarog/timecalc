401a320b35937311d9ec150ba7a0b9f2d4d6ab21
Arswarog <arswarog@yandex.ru>
2024-09-28 14:50:12 +0300

    add more correct layout builder

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,134 +1,60 @@
 import { ButtonCode } from '../types';
 import styles from './Keyboard.module.scss';
 import block from 'bem-css-modules';
+import { JSX, useMemo } from 'react';
+import { buttonsMeta, IButtonMeta, keyboardLayout } from './Keyboard.layout.ts';
+import { invert } from 'lodash';
 
 const b = block(styles, 'Keyboard');
+
+const buttonCodes: Record<string, ButtonCode> = invert(ButtonCode);
 
 export interface IKeyboardProps {
     onClick?: (value: ButtonCode) => void;
 }
 
 export function Keyboard({ onClick }: IKeyboardProps) {
+    const buttons = useMemo(() => buildButtons(buttonsMeta, keyboardLayout, onClick), []);
+    const gridTemplateAreas = useMemo(() => buildGridTemplateAreas(keyboardLayout), []);
+
     return (
-        <div className={b()}>
-            <button
-                className={b('button', { operator: true })}
-                onClick={() => onClick?.(ButtonCode.Plus)}
-                style={{ gridArea: 'plus' }}
-            >
-                +
-            </button>
-            <button
-                className={b('button', { operator: true })}
-                onClick={() => onClick?.(ButtonCode.Minus)}
-                style={{ gridArea: 'minus' }}
-            >
-                -
-            </button>
-            <button
-                className={b('button', { operator: true })}
-                onClick={() => onClick?.(ButtonCode.Multiply)}
-                style={{ gridArea: 'multiply' }}
-            >
-                &times;
-            </button>
-            <button
-                className={b('button', { operator: true })}
-                onClick={() => onClick?.(ButtonCode.Divide)}
-                style={{ gridArea: 'divide' }}
-            >
-                ÷
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol7)}
-                style={{ gridArea: 'symbol7' }}
-            >
-                7
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol8)}
-                style={{ gridArea: 'symbol8' }}
-            >
-                8
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol9)}
-                style={{ gridArea: 'symbol9' }}
-            >
-                9
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol4)}
-                style={{ gridArea: 'symbol4' }}
-            >
-                4
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol5)}
-                style={{ gridArea: 'symbol5' }}
-            >
-                5
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol6)}
-                style={{ gridArea: 'symbol6' }}
-            >
-                6
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol1)}
-                style={{ gridArea: 'symbol1' }}
-            >
-                1
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol2)}
-                style={{ gridArea: 'symbol2' }}
-            >
-                2
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol3)}
-                style={{ gridArea: 'symbol3' }}
-            >
-                3
-            </button>
-            <button
-                className={b('button')}
-                onClick={() => onClick?.(ButtonCode.Symbol0)}
-                style={{ gridArea: 'symbol0' }}
-            >
-                0
-            </button>
-            <button
-                className={b('button', { secondary: true })}
-                onClick={() => onClick?.(ButtonCode.SymbolDot)}
-                style={{ gridArea: 'symbolDot' }}
-            >
-                .
-            </button>
-            <button
-                className={b('button', { operator: true })}
-                style={{ gridArea: 'clear' }}
-            >
-                AC
-            </button>
-            <button
-                className={b('button', { enter: true })}
-                onClick={() => onClick?.(ButtonCode.Enter)}
-                style={{ gridArea: 'enter' }}
-            >
-                =
-            </button>
+        <div
+            className={b()}
+            style={{ gridTemplateAreas }}
+        >
+            {buttons}
         </div>
     );
+}
+
+function buildButtons(
+    buttonsMeta: { [key in ButtonCode]: IButtonMeta },
+    layout: ButtonCode[][],
+    onClick?: (btn: ButtonCode) => void,
+): JSX.Element[] {
+    const set = new Set<ButtonCode>(layout.flat(2));
+
+    return Object.entries(buttonsMeta)
+        .filter(([key]) => set.has(key as ButtonCode))
+        .map(([key, meta]) => {
+            const keyCode = key as ButtonCode;
+            const btnCode = buttonCodes[key];
+            return (
+                <button
+                    key={btnCode}
+                    className={b('button', {
+                        operator: meta.operator,
+                        enter: meta.enter,
+                    })}
+                    onClick={() => onClick?.(keyCode)}
+                    style={{ gridArea: btnCode }}
+                >
+                    {meta.text}
+                </button>
+            );
+        });
+}
+
+function buildGridTemplateAreas(layout: ButtonCode[][]): string {
+    return layout.map((row) => `"${row.map((item) => buttonCodes[item]).join(' ')}"`).join('\n');
 }
