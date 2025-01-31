979cdc3d729b7a7a78314073942562c4d11bbe67
arswarog <arswarog@yandex.ru>
2025-02-01 00:51:18 +0300

    add reatom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import './App.css';
+import { reatomContext } from '@reatom/npm-react';
+
 import { CalculatorUI } from './components/CalculatorUI';
 import { SiteTitle } from './components/SiteTitle';
 import { useCalc } from './hooks/useCalc';
+import { ctx } from './state';
 
 export function App() {
     const { display, onChange } = useCalc();
     return (
-        <>
+        <reatomContext.Provider value={ctx}>
             <SiteTitle />
             <CalculatorUI
                 display={display}
                 onChange={onChange}
             />
-        </>
+        </reatomContext.Provider>
     );
 }
