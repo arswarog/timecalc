378039fb6ec85235a6ba4df28d12d3481e215e41
arswarog <arswarog@yandex.ru>
2025-02-24 19:19:39 +0300

    refactor(errors): change ParserError

--- src/calc/parser/errors.ts
+++ src/calc/parser/errors.ts
@@ -1,18 +1,27 @@
-import { Token } from '../lexer';
-
 import { SyntaxContext } from './context';
 
 export class ParserError extends Error {
-    constructor(message: string, ctx: SyntaxContext, token: Token) {
-        super(ParserError.makeErrorMessage(message, ctx.getText(), token.start));
+    constructor(
+        public readonly error: string,
+        public readonly source: string,
+        public readonly position: number,
+    ) {
+        super(makePointedError(error, source, position));
     }
 
-    static makeErrorMessage(message: string, source: string, position: number): string {
-        const prefix = 'Line: ';
+    static fromCtx(error: string, ctx: SyntaxContext): ParserError {
+        const source = ctx.getText();
+        const position = ctx.getCurrentToken().start;
 
-        const line = source;
-        const pointer = ' '.repeat(position + prefix.length) + '^';
-
-        return `${message}\n${prefix}${line}\n${pointer}`;
+        return new ParserError(error, source, position);
     }
+}
+
+export function makePointedError(error: string, source: string, position: number): string {
+    const prefix = 'Line: ';
+
+    const line = source;
+    const pointer = ' '.repeat(position + prefix.length) + '^';
+
+    return `${error}\n${prefix}${line}\n${pointer}`;
 }
