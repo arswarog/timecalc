723cb236d74d67b095b84339c2d80466157f164b
Arswarog <arswarog@yandex.ru>
2025-07-29 08:15:56 +0300

    feat(pages/tokens-view): добавление страницы просмотра работы лексера

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router';
 
 import { AstViewPage } from '@src/pages/ast-view';
 import { CalculatorPage } from '@src/pages/calculator';
+import { TokensViewPage } from '@src/pages/tokens-view';
 
 import './App.css';
 
@@ -11,6 +12,10 @@ export function App() {
             <Route
                 path="ast"
                 element={<AstViewPage />}
+            />
+            <Route
+                path="tokens"
+                element={<TokensViewPage />}
             />
             <Route
                 path="*"
