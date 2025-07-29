ee9c983ce0ea265341d2fd3782fc11b8cf281c84
Arswarog <arswarog@yandex.ru>
2025-07-29 11:08:15 +0300

    refactor(features/evaluate/parser): изменение типа RootNode.epxression на AbstractNode

--- src/features/evaluate/lib/nodes/root.ts
+++ src/features/evaluate/lib/nodes/root.ts
@@ -1,14 +1,12 @@
 import { HighlightedError, PositionalError } from '../common';
 
 import { AbstractNode, NodeType } from './abstract';
-import { BinaryExpressionNode } from './binary-expression';
-import { ValueNode } from './value';
 
 export class RootNode extends AbstractNode {
     public readonly type = NodeType.Root;
 
     constructor(
-        public expression: BinaryExpressionNode | ValueNode | null,
+        public expression: AbstractNode,
         public source: string,
     ) {
         super();
