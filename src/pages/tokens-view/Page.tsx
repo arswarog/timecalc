7485c83b88aa890c27955aa97689ca0c1a117da0
Arswarog <arswarog@yandex.ru>
2025-07-29 08:15:56 +0300

    feat(pages/tokens-view): добавление страницы просмотра работы лексера

---
+++ src/pages/tokens-view/Page.tsx
@@ -0,0 +1,28 @@
+import { useAtom } from '@reatom/npm-react';
+import block from 'bem-css-modules';
+
+import { astAtom, astParsingErrorAtom } from '@src/features/evaluate';
+import { AstView } from '@src/widgets/ast-view';
+import { Screen } from '@src/widgets/calc-screen';
+import { ErrorView } from '@src/widgets/error-view';
+
+import styles from './Page.module.scss';
+const b = block(styles, 'TokensViewPage');
+
+export function TokensViewPage() {
+    const [ast] = useAtom(astAtom);
+    const [error] = useAtom(astParsingErrorAtom);
+
+    return (
+        <div className={b()}>
+            <h1 className={b('title')}>Просмотр AST</h1>
+            <div className={b('display')}>
+                <Screen />
+            </div>
+            <div className={b('ast')}>
+                {error && <ErrorView error={error} />}
+                {ast ? <AstView ast={ast} /> : 'Ошибка парсинга выражения'}
+            </div>
+        </div>
+    );
+}
