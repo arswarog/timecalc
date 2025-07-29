29ee263178e36117ffe412f4c27466652b7fb1e6
Arswarog <arswarog@yandex.ru>
2025-07-29 08:56:27 +0300

    fix(widgets/tokens-view): табличное отображение токенов

--- src/widgets/tokens-view/TokensView.tsx
+++ src/widgets/tokens-view/TokensView.tsx
@@ -1,9 +1,47 @@
-import { Token } from '@src/features/evaluate';
+import { Token, TokenType } from '@src/parser';
 
 interface TokensViewProps {
     tokens: Token[];
 }
 
+import block from 'bem-css-modules';
+
+import styles from './TokensView.module.scss';
+
+const b = block(styles, 'TokensView');
+
 export function TokensView({ tokens }: TokensViewProps) {
-    return <pre>{JSON.stringify(tokens, null, 4)}</pre>;
+    return (
+        <div className={b()}>
+            <table className={b('table')}>
+                <thead>
+                    <tr className={b('header')}>
+                        <th className={b('type')}>Тип</th>
+                        <th className={b('text')}>Текст</th>
+                        <th className={b('position')}>Позиция</th>
+                        <th className={b('position')}>Длина</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tokens.map((token) => (
+                        <tr
+                            key={token.start}
+                            className={b('row')}
+                        >
+                            <td className={b('type')}>{TokenType[token.type]}</td>
+                            <td className={b('text')}>
+                                "<span className={b('text-content')}>{token.text}</span>"
+                            </td>
+                            <td className={b('position')}>
+                                {token.start} - {token.end}
+                            </td>
+                            <td className={b('position')}>
+                                <i>{token.end - token.start}</i>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
