3d6d3e8212110172cf688c9087e0b9e64b3ccd43
Arswarog <arswarog@yandex.ru>
2025-07-30 06:17:03 +0300

    refactor(pages): переход страниц на DevLayout

--- src/pages/ast-view/Page.tsx
+++ src/pages/ast-view/Page.tsx
@@ -1,30 +1,18 @@
 import { useAtom } from '@reatom/npm-react';
-import block from 'bem-css-modules';
 
 import { astAtom, astParsingErrorAtom } from '@src/features/evaluate';
+import { DevLayout } from '@src/pages/dev-layout';
 import { AstView } from '@src/widgets/ast-view';
-import { Screen } from '@src/widgets/calc-screen';
 import { ErrorView } from '@src/widgets/error-view';
-import { Navbar } from '@src/widgets/navbar';
-
-import styles from './Page.module.scss';
-const b = block(styles, 'AstViewPage');
 
 export function AstViewPage() {
     const [ast] = useAtom(astAtom);
     const [error] = useAtom(astParsingErrorAtom);
 
     return (
-        <div className={b()}>
-            <h1 className={b('title')}>Просмотр AST</h1>
-            <div className={b('display')}>
-                <Screen />
-            </div>
-            <Navbar />
-            <div className={b('ast')}>
-                {error && <ErrorView error={error} />}
-                {ast ? <AstView ast={ast} /> : 'Ошибка парсинга выражения'}
-            </div>
-        </div>
+        <DevLayout title="Просмотр AST">
+            {error && <ErrorView error={error} />}
+            {ast ? <AstView ast={ast} /> : 'Ошибка парсинга выражения'}
+        </DevLayout>
     );
 }
