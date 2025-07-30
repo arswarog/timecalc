28ae5433977472f42e8ad43b1681aaac84c6c397
Лубяной Евгений <arswarog@users.noreply.github.com>
2025-07-30 07:02:27 +0300

    feat(pages/dev-layout): добавление шаблона для страниц отладки

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router';
 
 import { AstViewPage } from '@src/pages/ast-view';
 import { CalculatorPage } from '@src/pages/calculator';
+import { DevLayout } from '@src/pages/dev-layout';
 import { TokensViewPage } from '@src/pages/tokens-view';
 
 import './App.css';
@@ -9,14 +10,16 @@ import './App.css';
 export function App() {
     return (
         <Routes>
-            <Route
-                path="ast"
-                element={<AstViewPage />}
-            />
-            <Route
-                path="tokens"
-                element={<TokensViewPage />}
-            />
+            <Route element={<DevLayout />}>
+                <Route
+                    path="ast"
+                    element={<AstViewPage />}
+                />
+                <Route
+                    path="tokens"
+                    element={<TokensViewPage />}
+                />
+            </Route>
             <Route
                 path="*"
                 element={<CalculatorPage />}
--- src/pages/ast-view/Page.tsx
+++ src/pages/ast-view/Page.tsx
@@ -1,30 +1,17 @@
 import { useAtom } from '@reatom/npm-react';
-import block from 'bem-css-modules';
 
 import { astAtom, astParsingErrorAtom } from '@src/features/evaluate';
 import { AstView } from '@src/widgets/ast-view';
-import { Screen } from '@src/widgets/calc-screen';
 import { ErrorView } from '@src/widgets/error-view';
-import { Navbar } from '@src/widgets/navbar';
-
-import styles from './Page.module.scss';
-const b = block(styles, 'AstViewPage');
 
 export function AstViewPage() {
     const [ast] = useAtom(astAtom);
     const [error] = useAtom(astParsingErrorAtom);
 
     return (
-        <div className={b()}>
-            <h1 className={b('title')}>Просмотр AST</h1>
-            <div className={b('display')}>
-                <Screen />
-            </div>
-            <Navbar />
-            <div className={b('ast')}>
-                {error && <ErrorView error={error} />}
-                {ast ? <AstView ast={ast} /> : 'Ошибка парсинга выражения'}
-            </div>
-        </div>
+        <>
+            {error && <ErrorView error={error} />}
+            {ast ? <AstView ast={ast} /> : 'Ошибка парсинга выражения'}
+        </>
     );
 }
