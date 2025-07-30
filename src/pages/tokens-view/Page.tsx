05d9789d5890edcb82fb4afe92d173741752474d
Лубяной Евгений <arswarog@users.noreply.github.com>
2025-07-30 05:59:15 +0300

    feat(pages/tokens-view): табличное отображение токенов

---
+++ src/pages/tokens-view/Page.tsx
@@ -0,0 +1,28 @@
+import { useAtom } from '@reatom/npm-react';
+import block from 'bem-css-modules';
+
+import { astParsingErrorAtom, tokensAtom } from '@src/features/evaluate';
+import { Screen } from '@src/widgets/calc-screen';
+import { ErrorView } from '@src/widgets/error-view';
+import { TokensView } from '@src/widgets/tokens-view';
+
+import styles from './Page.module.scss';
+const b = block(styles, 'TokensViewPage');
+
+export function TokensViewPage() {
+    const [tokens] = useAtom(tokensAtom);
+    const [error] = useAtom(astParsingErrorAtom);
+
+    return (
+        <div className={b()}>
+            <h1 className={b('title')}>Просмотр токенов</h1>
+            <div className={b('display')}>
+                <Screen />
+            </div>
+            <div className={b('ast')}>
+                {error && <ErrorView error={error} />}
+                {tokens ? <TokensView tokens={tokens} /> : 'Ошибка парсинга выражения'}
+            </div>
+        </div>
+    );
+}
