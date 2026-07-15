c2cc57ff2d2ac8277a413013a3721e89cefc8334
Arswarog <arswarog@yandex.ru>
2026-07-15 18:11:22 +0300

    test(parser.parser.value): добавление в иерархию тестов раздела single value

--- src/parser/parser/parse-value.spec.ts
+++ src/parser/parser/parse-value.spec.ts
@@ -71,73 +71,75 @@ describe('Parser / Value', () => {
         });
     });
     describe('time', () => {
-        it('23s', () => {
-            // Arrange
-            const source = '23s';
+        describe('single values', () => {
+            it('23s', () => {
+                // Arrange
+                const source = '23s';
 
-            // Act
-            const ast = parse(source);
-            const result = ast.evaluate();
+                // Act
+                const ast = parse(source);
+                const result = ast.evaluate();
 
-            // Assert
-            expect(ast).toEqual(
-                new RootNode(
-                    new ValueNode(
-                        [createToken(TokenType.NumericLiteral, '23', 0)],
-                        createToken(TokenType.SecondLiteral, 's', 2),
+                // Assert
+                expect(ast).toEqual(
+                    new RootNode(
+                        new ValueNode(
+                            [createToken(TokenType.NumericLiteral, '23', 0)],
+                            createToken(TokenType.SecondLiteral, 's', 2),
+                        ),
+                        source,
                     ),
-                    source,
-                ),
-            );
-            expect(result).toEqual({
-                type: ValueType.Time,
-                value: 23,
+                );
+                expect(result).toEqual({
+                    type: ValueType.Time,
+                    value: 23,
+                });
             });
-        });
-        it('2m', () => {
-            // Arrange
-            const source = '2m';
+            it('2m', () => {
+                // Arrange
+                const source = '2m';
 
-            // Act
-            const ast = parse(source);
-            const result = ast.evaluate();
+                // Act
+                const ast = parse(source);
+                const result = ast.evaluate();
 
-            // Assert
-            expect(ast).toEqual(
-                new RootNode(
-                    new ValueNode(
-                        [createToken(TokenType.NumericLiteral, '2', 0)],
-                        createToken(TokenType.MinuteLiteral, 'm', 1),
+                // Assert
+                expect(ast).toEqual(
+                    new RootNode(
+                        new ValueNode(
+                            [createToken(TokenType.NumericLiteral, '2', 0)],
+                            createToken(TokenType.MinuteLiteral, 'm', 1),
+                        ),
+                        source,
                     ),
-                    source,
-                ),
-            );
-            expect(result).toEqual({
-                type: ValueType.Time,
-                value: 120,
+                );
+                expect(result).toEqual({
+                    type: ValueType.Time,
+                    value: 120,
+                });
             });
-        });
-        it('3h', () => {
-            // Arrange
-            const source = '3h';
+            it('3h', () => {
+                // Arrange
+                const source = '3h';
 
-            // Act
-            const ast = parse(source);
-            const result = ast.evaluate();
+                // Act
+                const ast = parse(source);
+                const result = ast.evaluate();
 
-            // Assert
-            expect(ast).toEqual(
-                new RootNode(
-                    new ValueNode(
-                        [createToken(TokenType.NumericLiteral, '3', 0)],
-                        createToken(TokenType.HourLiteral, 'h', 1),
+                // Assert
+                expect(ast).toEqual(
+                    new RootNode(
+                        new ValueNode(
+                            [createToken(TokenType.NumericLiteral, '3', 0)],
+                            createToken(TokenType.HourLiteral, 'h', 1),
+                        ),
+                        source,
                     ),
-                    source,
-                ),
-            );
-            expect(result).toEqual({
-                type: ValueType.Time,
-                value: 10800,
+                );
+                expect(result).toEqual({
+                    type: ValueType.Time,
+                    value: 10800,
+                });
             });
         });
     });
