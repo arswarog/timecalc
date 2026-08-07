e778c414981a9110f3503e3fae53995b2cfba0c7
Лубяной Евгений <arswarog@users.noreply.github.com>
2026-08-07 09:49:32 +0300

    chore(eslint): автоудаление неиспользуемых импортов (#50)
    Команды преименованы для соответствия частоты использования

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
 import globals from 'globals';
 import tsEslint from 'typescript-eslint';
 
@@ -28,15 +29,23 @@ export default tsEslint.config(
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
             react: reactPlugin,
+            'unused-imports': unusedImports,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
             'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-            '@typescript-eslint/no-unused-vars': [
-                'warn',
+
+            '@typescript-eslint/no-unused-vars': 'off',
+            'unused-imports/no-unused-imports': 'error',
+            'unused-imports/no-unused-vars': [
+                'error',
                 {
-                    caughtErrorsIgnorePattern: '^_',
+                    vars: 'all',
+                    args: 'after-used',
+                    ignoreRestSiblings: true,
+                    argsIgnorePattern: '^_',
                     varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
                 },
             ],
 
